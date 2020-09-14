31d865bc78b8d94b8919150ee60ffec286c2b7c4
Amar Aryal <amararyal878@gmail.com>
2020-09-14 14:27:30 +0545

    last commit

--- backend/twitter_like_app/twitter_like_app/settings.py
+++ backend/twitter_like_app/twitter_like_app/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 
 from pathlib import Path
 
+from .secrets import *
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -20,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '6m9$w1dd_54%wk96ek^9e@k(j#t8qgr)s-g183124da!exvol*'
+SECRET_KEY = SECRET_KEY
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
